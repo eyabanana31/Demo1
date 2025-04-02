09eb07124f55d72aa79614b9b9256481f181f50b
Andrea <andreaoreno98@gmail.com>
2025-04-02 15:34:39 +0800

    second commit

--- Demo1.java
+++ Demo1.java
@@ -2,6 +2,6 @@ public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Welcome to Git Branching Strategy Demo!");
         System.out.println("Your Name: Andrea  ”);
-        System.out.println(“Your” Capability:  );
+        System.out.println(“Your” Capability: Hybrid Cloud Database );
     }
 }
